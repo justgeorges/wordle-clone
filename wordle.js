b1cf437a38932a57597a1ccb0b8eb12337c880be
George Arbanas <george.arbanas@icloud.com>
2023-11-08 18:39:11 +0900

    created variable loading state

--- wordle.js
+++ wordle.js
@@ -6,6 +6,11 @@ async function init() {
   let currentGuess = "";
   let currentRow = 0;
 
+  const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+  const resObj = await res.json();
+  const word = resObj.word.toUpperCase();
+  setLoading(false);
+
   function addLetter(letter) {
     if (currentGuess.length < ANSWER_LENGTH) {
       // add letter to the end
@@ -58,6 +63,10 @@ async function init() {
 
 function isLetter(letter) {
   return /^[a-zA-z]$/.test(letter);
+}
+
+function setLoading(isLoading) {
+  loadingDiv.classList.toggle("show", isLoading);
 }
 
 init();
